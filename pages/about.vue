3c97472e051bd0f1fbbafec715088e5d47540bc7
sanditzz <kk.pon3@gmail.com>
2024-08-14 15:41:39 +0700

    About page

---
+++ pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-col items-center my-8">
+    <div class="flex flex-col items-center gap-4 mb-8">
+      <h1 class="text-4xl font-light text-center md:text-5xl">About For Farmers</h1>
+      <p>An open-source project aiming to help farmers.</p>
+    </div>
+
+    <div class="flex gap-4">
+      <a
+        class="block p-6 transition-all duration-300 bg-white rounded-lg shadow-sm hover:shadow-lg"
+        href="https://github.com/forfarmers"
+        target="_blank"
+        rel="noopener noreferrer"
+        >Github</a
+      >
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
